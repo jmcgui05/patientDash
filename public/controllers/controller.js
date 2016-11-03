44ef36824ac79e3c447bf2e57367320512d94ff8
jmcgui05 <johnmac81@gmail.com>
2016-11-02 19:58:47 -0500

    connect to db, config mongoose and format dates

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,4 +1,10 @@
 var patientApp = angular.module('patientApp', []);
+
 patientApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
   console.log('Hello from controller');
-}])
+
+  $http.get('/patientList').success(function(response) {
+    $scope.patientList = response;
+  });
+
+}]);
--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+var db = require('./config/db');
+
+
 
 app.use(express.static(__dirname + "/public"));
+
+app.get('/patientList', function(req, res) {
+  console.log('I got a GET request');
+
+  db.find({}, function(err, data) {
+    if (err) {
+      console.log(err);
+      return res.send(500, 'Something Went wrong with Retrieving data');
+    } else {
+      console.log(data[0].patient);
+      res.json(data);
+    }
+  });
+
+});
 
 app.listen(port);
 console.log('Server listening on port: ', port);
