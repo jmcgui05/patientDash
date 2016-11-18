8a7e1691e4aa45a73696e2a24425c35e1ded8736
jmcgui05 <johnmac81@gmail.com>
2016-11-17 20:33:42 -0600

    client side pagination

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,5 +1,12 @@
 var patientApp = angular.module('patientApp', []);
 
+patientApp.filter('offset', function() {
+  return function(input, start) {
+    start = parseInt(start, 10);
+    return input.slice(start);
+  };
+});
+
 patientApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
   console.log('Hello from controller');
 
@@ -17,6 +24,40 @@ patientApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
   $scope.updatePatient = function(){
     console.log('scope function called')
     $http.post('/patientList', $scope.patient);
+  };
+
+  $scope.itemsPerPage = 5;
+  $scope.currentPage = 0;
+  $scope.items = [];
+
+  for (var i=0; i<50; i++) {
+    $scope.items.push({
+      id: i, name: "name "+ i, description: "description " + i
+    });
   }
+
+  $scope.prevPage = function() {
+    if ($scope.currentPage > 0) {
+      $scope.currentPage--;
+    }
+  };
+
+  $scope.prevPageDisabled = function() {
+    return $scope.currentPage === 0 ? "disabled" : "";
+  };
+
+  $scope.pageCount = function() {
+    return Math.ceil($scope.items.length/$scope.itemsPerPage)-1;
+  };
+
+  $scope.nextPage = function() {
+    if ($scope.currentPage < $scope.pageCount()) {
+      $scope.currentPage++;
+    }
+  };
+
+  $scope.nextPageDisabled = function() {
+    return $scope.currentPage === $scope.pageCount() ? "disabled" : "";
+  };
 
 }]);
